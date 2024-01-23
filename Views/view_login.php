9c8b0179b6fd080aff303a304406822a793ed373
Jlawl1et <99320146+Jlawl1et@users.noreply.github.com>
2024-01-23 02:25:15 +0100

    View_Begin / view_End CSS/Views of Login / Sign Up page

--- Views/view_login.php
+++ Views/view_login.php
@@ -1,1 +1,35 @@
+<?php require "view_begin.php";?>
+<link rel="stylesheet" href="../Content/css/login.css">
+
+    <section class="connexion">
+        <div class="se_connecter">
+            <h1>Connexion</h1>
+        </div>
+
+         <div class="formulaire">
+
+            <form action="?controller=auth&action=login" method="post" >
+            <div class="form-group">
+                  <label class="p-label" for="createEmail">Email </label>
+                  <input type="email" name="email" id="createEmail" maxlength="60" value="" class="form-control input-lg">
+            </div>
+
+            <div class="form-group">
+                  <label class="p-label" for="createPassword">Mot de passe </label>
+                  <input type="password" name="password" id="createPassword" minlength="8" maxlength="64" class="form-control input-lg">
+            </div>
+
+            <p><a href="?controller=auth&action=form_oublimdp">Mot de passe oublié ? </a></p> <br>
+            <div class="d-flex justify-content-end p-2">
+                    <input type="submit" class="btn btn-md p_btn" value="Connexion">
+            </div>
+            </br>
+
+            <p>Tu n'as de compte ? <a href="?controller=auth&action=form_signup">Inscris-toi</a></p>
+            </form>
+        </div>
+    </section>
+    </main>
+
+<?php require "view_end.php";?>
 
---
+++ Views/view_signup.php
@@ -0,0 +1,50 @@
+<?php if (isset($_SESSION["connected"]) && $_SESSION["connected"]==True) : ?>
+  <?php require "view_begin_connected.php";?>
+<?php else : ?>
+  <?php require "view_begin.php";?>
+<?php endif ?>
+<link rel="stylesheet" href="../Content/css/signup.css">
+
+<section class="inscription">
+  <h1 class="text-center mb-0">Formulaire d'inscription</h1>
+  <div class="formulaire">
+  <form action="?controller=auth&action=signup" method="post" >
+
+    <div class="form-group ">
+      <label class="p-label" for="name">Nom *</label>
+      <input type="text" name="name" id="name" maxlength="40" value="" class="form-control input-lg ">
+    </div>
+
+    <div class="form-group ">
+      <label class="p-label" for="firstname">Prénom *</label>
+      <input type="text" name="firstname" id="firstname" maxlength="40" value="" class="form-control input-lg">
+    </div>
+
+
+    <div class="form-group">
+        <label class="p-label" for="createEmail">Email *</label>
+        <input type="email" name="email" id="createEmail" maxlength="60" value="" class="form-control input-lg">
+    </div>
+
+      <div class="form-group">
+          <label class="p-label" for="createPassword">Mot de passe *</label>
+          <input type="password" name="password" id="createPassword" minlength="8" maxlength="64" class="form-control input-lg">
+      </div>
+
+      <div class="form-group ">
+        <label class="p-label" for="confirmPassword">Confirme ton mot de passe *</label>
+        <input type="password" name="confirmPassword" id="confirmPassword" maxlength="64" class="form-control input-lg ">
+      </div>
+
+    <div class="d-flex justify-content-end p-2">
+        <input type="submit" class="btn btn-md p_btn" value="S'identifier">
+    </div>
+
+  </form>
+  </div>
+</section>
+
+</main>
+</br>
+
+<?php require "view_end.php";?>
