c7f48b99a699ff639b3712c847c4bcd46de3ce97
Jefferson Paillot <99320146+Jlawl1et@users.noreply.github.com>
2024-01-25 01:44:10 +0100

    Front site (#3)
    * view_movie
    
    * front_site : remove content img and update new logo uspn
    
    front_site : remove content img and update new logo uspn
    
    * view_movie.php
    
    ---------
    
    Co-authored-by: Gupta Varun Vedic <guptagivarun@gmail.com>

--- Views/view_end.php
+++ Views/view_end.php
@@ -8,7 +8,7 @@
 <footer>
     <div class="contenu-footer">
         <a href="https://www.univ-paris13.fr/" class="logo-univ">
-            <img style="border-radius: 4px;" src="../Content/img/logo_uspn.jpg" alt="Logo Université Sorbonne Paris Nord" width="100" height="50">
+            <img style="border-radius: 4px;" src="../Content/img/Logo-USPN-2023.jpg" alt="Logo Université Sorbonne Paris Nord" width="100" height="50">
         </a>
 
         <div class="aide">
@@ -18,7 +18,7 @@
             <a href="#" target="_blank" rel="noopener noreferrer">Nous contacter</a>
         </div>
 
-        <div class="bde">
+        <div class="sc">
             <h5>Space Ciné</h5>
             <a href="#">Qui sommes-nous ?</a>
         </div>
