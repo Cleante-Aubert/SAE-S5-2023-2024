635df8a18c1ad7ad93f78379e5112b763547cf1e
Jlawl1et <99320146+Jlawl1et@users.noreply.github.com>
2024-01-25 01:38:41 +0100

    view_movie.php

--- Views/view_movie.php
+++ Views/view_movie.php
@@ -1,98 +1,114 @@
 <?php require "view_begin.php";?>
 
     <!DOCTYPE html>
-    <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
         <title>Movies</title>
     </head>
-    <br>
+            <br>
 
-    <div class="row" id="movie" >
-
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1 class="text-center">Napoleon</h1>
-            </div>
-        </div>
-    </div>
-
-        <div class="container mt-5">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card rounded" style="width: 200px; height: 300px;">
-                        <img src="../Content/img/Napoleon.jpg" class="card-img" alt="Card Image">
-                    </div>
-                </div>
-                <div class="col-md-6 mx-auto">
+                <div class="container">
                     <div class="row">
                         <div class="col">
-                            <p>17 mars 2023 en salle | 1h 30min | Comédie dramatique</p>
-                            <p>De Norman Thavaud</p>
-                            <p>Avec Norman Thavaud, Mallory Wanecque, Julia Piaton</p>
-                            <h1>Synopsis</h1>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor negro ut labore et dolore magna aliqua…</p>
+                            <h1 class="text-center">Napoleon</h1>
+                            <!-- Star Rating -->
+                            <div class="text-center">
+                                <span class="text-warning" style="font-size: 2em;">&#9733;</span>
+                                <span class="text-warning" style="font-size: 2em;">&#9733;</span>
+                                <span class="text-warning" style="font-size: 2em;">&#9733;</span>
+                                <span class="text-warning" style="font-size: 2em;">&#9733;</span>
+                                <span class="text-secondary" style="font-size: 2em;">&#9733;</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3">
-                    <div class="container mt-4">
-                        <h2 class="text-center mb-4">Casting</h2>
 
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card" style="width: 50px; height: 50px;">
-                                    <img src="../Content/img/Kemar.png" alt="Image 1" class="card-img-top img-fluid rounded" style="width: 50px; height: 50px;">
-                                </div>
+                <div class="container mt-5">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="card rounded" style="width: 200px; height: 300px;">
+                                <img src="../Content/img/Napoleon.jpg" class="card-img" alt="Card Image">
                             </div>
-                            <div class="col-12 mt-3">
-                                <div class="card" style="width: 50px; height: 50px;">
-                                    <img src="../Content/img/Kemar.png" alt="Image 2" class="card-img-top img-fluid rounded" style="width: 50px; height: 50px;">
-                                </div>
-                            </div>
-                            <div class="col-12 mt-3">
-                                <div class="card" style="width: 50px; height: 50px;">
-                                    <img src="../Content/img/Kemar.png" alt="Image 3" class="card-img-top img-fluid rounded" style="width: 50px; height: 50px;">
+                        </div>
+                        <div class="col-md-6 mx-auto">
+                            <div class="row">
+                                <div class="col">
+                                    <p>17 mars 2023 en salle | 1h 30min | Comédie dramatique</p>
+                                    <p>De Norman Thavaud</p>
+                                    <p>Avec Norman Thavaud, Mallory Wanecque, Julia Piaton</p>
+                                    <h1>Synopsis</h1>
+                                    <p>Pendant ce temps, Maximilien de Robespierre fait régner la Terreur mais les députés de la Convention le mettent en cause et le chassent en juillet 1794 : Robespierre tente alors de se suicider mais échoue, et termine guillotiné. Les dirigeants français mettent fin à la Terreur et libèrent les prisonniers, dont Joséphine de Beauharnais, une veuve aristocrate. En octobre 1795, Barras confie à Bonaparte la mission de mater l'insurrection royaliste de vendémiaire : Bonaparte fait tirer aux canons en pleine rue, ce qui lui permet de gagner en popularité dans le camp républicain.</p>
                                 </div>
                             </div>
                         </div>
+                        <div class="col-md-3">
+                            <div class="container mt-4">
+                                <h2 class="text-center mb-4">Casting</h2>
 
-                        <div class="row mt-3">
-                            <div class="col-12">
-                                <div class="card" style="width: 50px; height: 50px;">
-                                    <img src="../Content/img/Kemar.png" alt="Image 4" class="card-img-top img-fluid rounded" style="width: 50px; height: 50px;">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="card" style="width: 120px; height: 120px;">
+                                            <img src="../Content/img/Kemar.png" alt="Image 1" class="card-img-top img-fluid rounded" style="width: 120px; height: 120px;">
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="card" style="width: 120px; height: 120px;">
+                                            <img src="../Content/img/Kemar.png" alt="Image 2" class="card-img-top img-fluid rounded" style="width: 120px; height: 120px;">
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-12 mt-3">
-                                <div class="card" style="width: 50px; height: 50px;">
-                                    <img src="../Content/img/Kemar.png" alt="Image 5" class="card-img-top img-fluid rounded" style="width: 50px; height: 50px;">
-                                </div>
-                            </div>
-                            <div class="col-12 mt-3">
-                                <div class="card" style="width: 50px; height: 50px;">
-                                    <img src="../Content/img/Kemar.png" alt="Image 6" class="card-img-top img-fluid rounded" style="width: 50px; height: 50px;">
+
+                                <div class="row mt-3">
+                                    <div class="col-6">
+                                        <div class="card" style="width: 120px; height: 120px;">
+                                            <img src="../Content/img/Kemar.png" alt="Image 3" class="card-img-top img-fluid rounded" style="width: 120px; height: 120px;">
+                                        </div>
+                                    </div>
+                                    <div class="col-6">
+                                        <div class="card" style="width: 120px; height: 120px;">
+                                            <img src="../Content/img/Kemar.png" alt="Image 4" class="card-img-top img-fluid rounded" style="width: 120px; height: 120px;">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </br>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-center">Bande annonce</h1>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    </br>
-    </br>
-    </br>
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <!-- Utilisez la classe "rounded" de Bootstrap pour les coins arrondis -->
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/C6qvbnbmflc" frameborder="0" allowfullscreen class="mx-auto d-block rounded"></iframe>
+                    </div>
+                </div>
+            </div>
+
+            </br>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-center">Section commentaire</h1>
+                    </div>
+                </div>
+            </div>
+
+    </html>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
-    </body>
-    </html>
-
 
 <?php require "view_end.php";?>
