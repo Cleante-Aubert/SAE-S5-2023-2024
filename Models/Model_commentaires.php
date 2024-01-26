ab6f2c3fafaabefe215ab72b7da018af21dab112
Cléante Aubert <cleante95@gmail.com>
2024-01-26 23:31:32 +0100

    Modifier commentaire Model.php

--- Models/Model_commentaires.php
+++ Models/Model_commentaires.php
@@ -18,6 +18,20 @@ class Model_commentaires extends Model {
         return $requete->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function update_commentaire($ID_user,$Com){
+        $bd = $this->getBd();
+        $requete = $bd>prepare('UPDATE critiques SET Commentaires = :com WHERE UtilisateurID = :id ');
+
+        //Remplacement des marqueurs de place par les valeurs
+        $requete->bindValue(':id', $ID_user);
+        $requete->bindValue(':com', $Com);
+
+        //Exécution de la requête
+        $requete->execute();
+
+        return (bool) $requete->rowCount();
+    }
+
 
 
 
