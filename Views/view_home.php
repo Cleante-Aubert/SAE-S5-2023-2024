11f52416ab450a1e435964425e07532ba680702e
Jlawl1et <99320146+Jlawl1et@users.noreply.github.com>
2024-01-29 02:29:06 +0100

    Comments section front

--- Views/view_home.php
+++ Views/view_home.php
@@ -10,29 +10,8 @@
 <!-- Section 2 : Films à la une et Dernières sorties -->
 <?php require "view_movie_list.php"; ?>
 
-<!-- Section 3 : Actualités liées au cinéma -->
-<section class="header-section">
-    <p class="actualites">Actualités liées au cinéma</p>
-    <div class="scrolling-container">
-    <?php
-    //foreach ($actualitesCinema as $actualite): ?>
-        <!-- Afficher les détails des actualités -->
-        <!--<div class="actualite"> -->
-            <!--<h3><?php //echo $actualite["Titre"]; ?></h3>-->
-            <!-- Ajoutez d'autres détails si nécessaire -->
-        <!--</div> -->
-    <?php //endforeach; ?>
-    </div>
-</section>
-<div class="container">
-    <div class="d-flex">
-        <div class="nuage mx-6">Actualité 1</div>
-        <div class="nuage mx-6">Actualité 2</div>
-        <div class="nuage mx-6">Actualité 3</div>
-        <div class="nuage mx-6">Actualité 4</div>
-        <div class="nuage mx-6">Actualité 5</div>
-    </div>
-</div>
+
+
 <!-- Section 4 : Bandes annonces de films -->
 <section >
     <p class="header-section">Bandes annonces de films</p>
